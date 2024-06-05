814162a2a959c63fd3550f50c387ffcda7ed9446
Akihiro Yamada <153415348+mountainpaddy040818@users.noreply.github.com>
2024-06-05 13:52:53 +0000

    second commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "28b24349cd1745569e219f60f9680e9f.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
