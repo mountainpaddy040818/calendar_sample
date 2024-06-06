7fb659bbbe1912295ece14c31316ae7cc19d1fa4
Akihiro Yamada <153415348+mountainpaddy040818@users.noreply.github.com>
2024-06-06 11:09:26 +0000

    カレンダー

--- app/javascript/packs/calendar.js
+++ app/javascript/packs/calendar.js
@@ -1,12 +1,21 @@
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from "@fullcalendar/list";
 
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
   var calendar = new Calendar(calendarEl, {
-    plugins: [dayGridPlugin],
+    plugins: [dayGridPlugin, listPlugin],
     initialView: 'dayGridMonth',
-    events: '/events'
+    locale: "jp",
+    events: '/events', 
+    windowResize: function () {
+        if (window.innerWidth < 991.98) {
+            calendar.changeView('listMonth');
+        } else {
+            calendar.changeView('dayGridMonth');
+        }
+    },
   });
 
   calendar.render();
